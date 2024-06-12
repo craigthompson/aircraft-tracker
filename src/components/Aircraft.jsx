4ca1eec9647446d601c01fa585dda49a0cf8bfd0
Craig <craigthompson85@gmail.com>
2024-06-12 13:42:05 -0600

    Added full jsdoc to Aircraft component

--- src/components/Aircraft.jsx
+++ src/components/Aircraft.jsx
@@ -11,9 +11,84 @@ import { metersToFeet } from "../../utils/distance.js";
 import { IoMdAirplane } from "react-icons/io";
 
 /**
- * Aircraft map marker at the given geographical coordinates.
+ * Aircraft map marker at the given geographical coordinates
+ * with popup showing additional given aircraft details.
  *
  * @component
+ * @param {Object} props - The component given properties
+ * @param {Number} props.aircraftId - The unique primary key of the aircraft
+ *   in the database.
+ * @param {String} props.icao24 - Unique ICAO 24-bit address of the
+ *   transponder in hex string representation. (up to 12 chars)
+ * @param {String} props.callsign - Callsign of the vehicle (8 chars). Can be
+ *   null if no callsign has been received.
+ * @param {String} props.originCountry - Country name inferred from the ICAO
+ *   24-bit address. (up to 100 chars)
+ * @param {Integer} props.timePosition - Unix timestamp (seconds) for the
+ *     last position update.
+ *   Can be null if no position report was received by OpenSky within the
+ *     past 15s.
+ * @param {Integer} props.lastContact - Unix timestamp (seconds) for the last
+ *     update in general.
+ *   This field is updated for any new, valid message received from the
+ *     transponder.
+ * @param {Float} props.longitude - WGS-84 longitude in decimal degrees.
+ *   Can be null.
+ * @param {Float} props.latitude - WGS-84 latitude in decimal degrees.
+ *   Can be null.
+ * @param {Float} props.baroAltitude - Barometric altitude in meters.
+ *   Can be null.
+ * @param {Boolean} props.onGround - Boolean value which indicates if the
+ *     position was retrieved from a surface position report.
+ *   Seems to be determined on OpenSky's side by the velocity of the
+ *     aircraft. If fast enough, < ~50kts, considered to be on the ground.
+ * @param {Float} props.velocity - Velocity over ground in m/s.
+ *   Can be null.
+ * @param {Float} props.trueTrack - True track in decimal degrees clockwise
+ *     from north (north=0°).
+ *   Can be null.
+ * @param {Float} props.verticalRate - Vertical rate in m/s.
+ *   A positive value indicates that the airplane is climbing, a negative
+ *     value indicates that it descends.
+ *   Can be null.
+ * @param {Integer} props.sensors - IDs of the receivers which contributed to
+ *     this state vector.
+ *   Is null if no filtering for sensor was used in the request.
+ * @param {Float} props.geoAltitude - Geometric altitude in meters.
+ *   Can be null.to
+ * @param {String} props.squawk - The transponder code aka Squawk.
+ *   Can be null. (up to 4 chars)
+ * @param {Boolean} props.spi - Whether flight status indicates special
+ *   purpose indicator.
+ * @param {Integer} props.positionSource - Origin of this state’s position:
+ *   0 = ADS-B
+ *   1 = ASTERIX
+ *   2 = MLAT
+ *   3 = FLARM
+ * @param {Integer} props.vehicleCategory - Aircraft category:
+ *   0  = No information at all
+ *   1  = No ADS-B Emitter Category Information
+ *   2  = Light (< 15500 lbs)
+ *   3  = Small (15500 to 75000 lbs)
+ *   4  = Large (75000 to 300000 lbs)
+ *   5  = High Vortex Large (aircraft such as B-757)
+ *   6  = Heavy (> 300000 lbs)
+ *   7  = High Performance (> 5g acceleration and 400 kts)
+ *   8  = Rotorcraft
+ *   9  = Glider / sailplane
+ *   10 = Lighter-than-air
+ *   11 = Parachutist / Skydiver
+ *   12 = Ultralight / hang-glider / paraglider
+ *   13 = Reserved
+ *   14 = Unmanned Aerial Vehicle
+ *   15 = Space / Trans-atmospheric vehicle
+ *   16 = Surface Vehicle – Emergency Vehicle
+ *   17 = Surface Vehicle – Service Vehicle
+ *   18 = Point Obstacle (includes tethered balloons)
+ *   19 = Cluster Obstacle
+ *   20 = Line Obstacle
+ *
+ * @returns {JSX.Element} The rendered map marker component.
  */
 const Aircraft = ({
   icao24,
