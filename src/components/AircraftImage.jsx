b99d49bc9db2e84bf84175c82f181b14d3d2d5a6
Craig <craigthompson85@gmail.com>
2024-06-14 13:31:30 -0600

    Added transparent background behind aircraft photo credit and link icon

--- src/components/AircraftImage.jsx
+++ src/components/AircraftImage.jsx
@@ -31,39 +31,37 @@ function AircraftImage({ icao24 }) {
 
   if (imgUrl) {
     return (
-      <div class="relative">
+      <div class="relative rounded-xl overflow-hidden">
         {/*loading indicator */}
         <a href={planespotterLinkUrl}>
           {!isImageLoaded && <div>Loading...</div>}
           <img
-            className={`p-0.5 rounded-xl drop-shadow-md ${
-              isImageLoaded ? "" : "hidden"
-            }`}
+            className={`drop-shadow-md ${isImageLoaded ? "" : "hidden"}`}
             src={imgUrl}
             alt={`Photo of `} // TODO: change alt text
             onLoad={handleImageLoad}
           />
           {isImageLoaded && (
-            <span
-              className="absolute bottom-1.5 left-2 text-slate-50 text-xs"
-              style={{
-                filter: `drop-shadow(2px 2px 3px rgb(0 0 0 / 0.2)) 
+            <div className="absolute inset-x-0 bottom-0 h-5 bg-slate-900/30 ">
+              <span
+                className="absolute left-1.5 bottom-0.5 text-slate-50 text-xs"
+                style={{
+                  filter: `drop-shadow(2px 2px 3px rgb(0 0 0 / 0.2)) 
+                    drop-shadow(1px 1px 1px rgb(0 0 0 / 0.4))
+                    drop-shadow(-1px -1px 1px rgb(0 0 0 / 0.25))`,
+                }}
+              >
+                {`© ${photographer}`}
+              </span>
+              <FaExternalLinkAlt
+                className="absolute right-1.5 bottom-1 text-slate-50 text-xs"
+                style={{
+                  filter: `drop-shadow(2px 2px 3px rgb(0 0 0 / 0.2)) 
                   drop-shadow(1px 1px 1px rgb(0 0 0 / 0.4))
                   drop-shadow(-1px -1px 1px rgb(0 0 0 / 0.25))`,
-              }}
-            >
-              {`© ${photographer}`}
-            </span>
-          )}
-          {isImageLoaded && (
-            <FaExternalLinkAlt
-              className="absolute bottom-2 right-2 text-slate-50 text-xs"
-              style={{
-                filter: `drop-shadow(2px 2px 3px rgb(0 0 0 / 0.2)) 
-                  drop-shadow(1px 1px 1px rgb(0 0 0 / 0.4))
-                  drop-shadow(-1px -1px 1px rgb(0 0 0 / 0.25))`,
-              }}
-            />
+                }}
+              />
+            </div>
           )}
         </a>
       </div>
