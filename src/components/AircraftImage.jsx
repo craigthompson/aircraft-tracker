d3dd40f4b163cd3c6702eb9086e1ea9dbcfe05b7
Craig <craigthompson85@gmail.com>
2024-06-14 12:28:50 -0600

    Added photographer credit and Planespotter link to aircraft image in the popup

--- src/components/AircraftImage.jsx
+++ src/components/AircraftImage.jsx
@@ -1,35 +1,71 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 function AircraftImage({ icao24 }) {
-  const [imgUrl, setImgUrl] = useState([]);
+  const [imgUrl, setImgUrl] = useState(null);
+  const [photographer, setPhotographer] = useState(null);
+  const [planespotterLinkUrl, setPlanespotterLinkUrl] = useState(null);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const getUrl = async () => {
-    const response = await axios.get(
-      `https://api.planespotters.net/pub/photos/hex/${icao24}`
-    );
-    console.log(response.data);
-    return response.data?.photos?.[0]?.thumbnail_large?.src ?? null;
-  };
-
-  useEffect(() => {
-    const getUrl = async () => {
+    try {
       const response = await axios.get(
         `https://api.planespotters.net/pub/photos/hex/${icao24}`
       );
-      setImgUrl(response.data?.photos?.[0]?.thumbnail_large?.src ?? null);
-    };
+      setImgUrl(response?.data?.photos?.[0]?.thumbnail_large?.src ?? null);
+      setPhotographer(response?.data?.photos?.[0]?.photographer ?? "");
+      setPlanespotterLinkUrl(response?.data?.photos?.[0]?.link ?? "");
+    } catch (error) {
+      console.error("Error fetching image:", error);
+    }
+  };
+
+  useEffect(() => {
     getUrl();
   }, []);
 
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
   if (imgUrl) {
     return (
-      <img
-        className="p-0.5 rounded-xl drop-shadow"
-        src={imgUrl}
-        alt={"Aircraft Image"}
-      />
-    ); // TODO: change alt text
+      <div class="relative">
+        {/*loading indicator */}
+        <a href={planespotterLinkUrl}>
+          {!isImageLoaded && <div>Loading...</div>}
+          <img
+            className={`p-0.5 rounded-xl drop-shadow-md ${
+              isImageLoaded ? "" : "hidden"
+            }`}
+            src={imgUrl}
+            alt={`Photo of `} // TODO: change alt text
+            onLoad={handleImageLoad}
+          />
+          {isImageLoaded && (
+            <span
+              className="absolute bottom-1.5 left-2 text-slate-50 text-xs"
+              style={{
+                filter: `drop-shadow(1px 2px 2px rgb(0 0 0 / 0.35)) 
+                  drop-shadow(0px 1px 1px rgb(0 0 0 / 0.25))`,
+              }}
+            >
+              {`© ${photographer}`}
+            </span>
+          )}
+          {isImageLoaded && (
+            <FaExternalLinkAlt
+              className="absolute bottom-2 right-2 text-slate-50 text-xs"
+              style={{
+                filter: `drop-shadow(1px 2px 2px rgb(0 0 0 / 0.35)) 
+                  drop-shadow(0px 1px 1px rgb(0 0 0 / 0.25))`,
+              }}
+            />
+          )}
+        </a>
+      </div>
+    );
   } else {
     return <></>;
   }
