85ec2e470f98bf7cd4bae2304310e30d4521ca7e
Craig <craigthompson85@gmail.com>
2024-06-15 10:48:43 -0600

    Added external link icon to callsign link to Flightaware in popup details

--- src/components/CallsignFlightDetailsLink.jsx
+++ src/components/CallsignFlightDetailsLink.jsx
@@ -1,3 +1,5 @@
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 function CallsignFlightDetailsLink({ callsign }) {
   return (
     callsign != null && (
@@ -7,8 +9,10 @@ function CallsignFlightDetailsLink({ callsign }) {
           <a
             href={`https://www.flightaware.com/live/flight/${callsign.toUpperCase()}`}
             target="_blank"
+            className="inline-flex items-center"
           >
-            {callsign.toUpperCase()}
+            {`${callsign.toUpperCase()} `}{" "}
+            <FaExternalLinkAlt className="ml-1 text-xs" />
           </a>
         </td>
       </>
