721fe5876c4e0dfef5a7e27d39a8e0ddb2986176
Craig <craigthompson85@gmail.com>
2024-06-17 13:42:38 -0600

    Removed unneeded console log

--- src/socket.js
+++ src/socket.js
@@ -3,10 +3,6 @@ import socketConfig from "../config/socket.config";
 
 const URL = (() => {
   if (import.meta.env.MODE === "development") {
-    console.log(
-      "Frontend connecting to:",
-      `${socketConfig.SOCKET_DEV_URL_DOMAIN}:${socketConfig.SOCKET_PORT}`
-    );
     return `${socketConfig.SOCKET_DEV_URL_DOMAIN}:${socketConfig.SOCKET_PORT}`;
   } else {
     return `${socketConfig.SOCKET_PROD_URL_DOMAIN}:${socketConfig.SOCKET_PORT}`;
