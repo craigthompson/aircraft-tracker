c92a387e0f622546ea1b3917ad9dce33a2414002
Craig <craigthompson85@gmail.com>
2024-06-24 11:45:16 -0600

    Added try-catch blocks around functional blocks to catch errors that may occur

--- database/watchedAircraft.js
+++ database/watchedAircraft.js
@@ -4,33 +4,46 @@ import { queryWatchedAircraft } from "./queries.js";
 import chalk from "chalk";
 
 export const upsertWatchedAircraft = async (aircraft) => {
-  const newWatchedAircraft = await WatchedAircraft.upsert({
-    icao24: aircraft.icao24,
-    callsign: aircraft.callsign,
-    flightStatus: aircraft.flightStatus,
-    departureAirport: aircraft.departureAirport,
-    arrivalAirport: aircraft.arrivalAirport,
-    airlineLogoUrl: aircraft.airlineLogoUrl,
-  });
+  try {
+    const newWatchedAircraft = await WatchedAircraft.upsert({
+      icao24: aircraft.icao24,
+      callsign: aircraft.callsign,
+      flightStatus: aircraft.flightStatus,
+      departureAirport: aircraft.departureAirport,
+      arrivalAirport: aircraft.arrivalAirport,
+      airlineLogoUrl: aircraft.airlineLogoUrl,
+    });
 
-  // await db.close();  // TODO: remove if not needed
-  return newWatchedAircraft;
+    return newWatchedAircraft;
+  } catch (error) {
+    console.error("Error during upserting watched aircraft:", error);
+    return null;
+  }
 };
 
 export const scrapeWatchedFlightsStatus = async () => {
-  const watchedFlights = await queryWatchedAircraft();
-  await Promise.all(
-    watchedFlights.map(async (flight) => {
-      const scrapedFlight = await scrapeGivenWatchedFlightStatus(flight);
-    })
-  );
-  // await db.close();  // TODO: remove if not needed
-  return watchedFlights;
+  try {
+    const watchedFlights = await queryWatchedAircraft();
+    await Promise.all(
+      watchedFlights.map(async (flight) => {
+        const scrapedFlight = await scrapeGivenWatchedFlightStatus(flight);
+      })
+    );
+
+    return watchedFlights;
+  } catch (error) {
+    console.error("Error during scraping watched aircraft:", error);
+    return null;
+  }
 };
 
 export const scrapeGivenWatchedFlightStatus = async (flight) => {
   try {
-    console.log(chalk.cyanBright(`[Scraper Bot] `), "Flight:", flight);
+    console.log(
+      chalk.cyanBright(`[Scraper Bot] `),
+      "Scraping for flight:",
+      flight
+    );
     const flightDetails = await scrapeFlightData(flight.callsign);
     flightDetails["callsign"] = flight.callsign;
     flightDetails["watchId"] = flight.watchId;
@@ -53,36 +66,47 @@ export const scrapeGivenWatchedFlightStatus = async (flight) => {
     return upsertedWatchedFlight;
   } catch (error) {
     console.error("Error during scraping for flight:", flight.callsign, error);
+    return null;
   }
 };
 
 export const upsertWatchedAircraftFlightDetails = async (flight) => {
-  const newWatchedAircraftFlightDetails = await WatchedAircraft.upsert({
-    callsign: flight.callsign,
-    flightStatus: flight.flightStatus,
-    departureAirport: flight.departureAirport,
-    arrivalAirport: flight.arrivalAirport,
-    airlineLogoUrl: flight.airlineLogoUrl,
-  });
+  try {
+    const newWatchedAircraftFlightDetails = await WatchedAircraft.upsert({
+      callsign: flight.callsign,
+      flightStatus: flight.flightStatus,
+      departureAirport: flight.departureAirport,
+      arrivalAirport: flight.arrivalAirport,
+      airlineLogoUrl: flight.airlineLogoUrl,
+    });
 
-  return newWatchedAircraftFlightDetails[0];
+    return newWatchedAircraftFlightDetails[0];
+  } catch (error) {
+    console.error("Error in upserting watched flight details:", error);
+    return null;
+  }
 };
 
 export const createWatchedAircraft = async (callsign) => {
-  const watchedAircraft = {
-    callsign: callsign,
-    flightStatus: null,
-    departureAirport: null,
-    arrivalAirport: null,
-    airlineLogoUrl: null,
-  };
-  const newWatchedAircraft = await scrapeGivenWatchedFlightStatus(
-    watchedAircraft
-  );
-  // const watchedAircraft = await WatchedAircraft.findAll({});
-  console.log("New watched aircraft:", newWatchedAircraft);
+  try {
+    const watchedAircraft = {
+      callsign: callsign,
+      flightStatus: null,
+      departureAirport: null,
+      arrivalAirport: null,
+      airlineLogoUrl: null,
+    };
+    const newWatchedAircraft = await scrapeGivenWatchedFlightStatus(
+      watchedAircraft
+    );
+    // const watchedAircraft = await WatchedAircraft.findAll({});
+    console.log("New watched aircraft:", newWatchedAircraft);
+  } catch (error) {
+    console.error(
+      "Error while trying to scrape and add watched flight.",
+      error
+    );
+  }
   const watchedFlights = await queryWatchedAircraft();
   return watchedFlights;
 };
-
-// await scrapeWatchedFlightsStatus(); // TODO: remove after dev
