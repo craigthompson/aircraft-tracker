b19c9f3325d4fe2a4ad012c700f36e1c54a38cb5
Craig <craigthompson85@gmail.com>
2024-06-26 09:04:56 -0600

    Made the watched flight delete button change color when hovered

--- src/components/WatchFlightDeleteButton.jsx
+++ src/components/WatchFlightDeleteButton.jsx
@@ -3,7 +3,10 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 function WatchFlightDeleteButton({ clickedDeleteButton }) {
   return (
     <div className="inline-block content-start">
-      <button onClick={clickedDeleteButton}>
+      <button
+        onClick={clickedDeleteButton}
+        className="text-secondary-500 hover:text-primary-600 hover:shadow hover"
+      >
         <RiDeleteBin6Line className="text-lg" />
       </button>
     </div>
