72da54dc131b3af20aeab45830d765eada38bee2
Craig <craigthompson85@gmail.com>
2024-06-28 15:33:16 -0600

    Changed socket URL back to local host

--- config/socket.config.js
+++ config/socket.config.js
@@ -4,9 +4,10 @@ import os from "node:os";
 
 const socketConfig = {
   SOCKET_PORT: 3000,
-  SOCKET_PROD_URL_DOMAIN: "REPLACE-WITH-DEPLOYED-DOMAIN-URL", // TODO: After deploying, set this to correct value
+  SOCKET_PROD_URL_DOMAIN: "REPLACE-WITH-DEPLOYED-DOMAIN-URL", // TODO: When deploying, set this to correct value
   SOCKET_DEV_URL_DOMAIN: "http://localhost",
-  // SOCKET_DEV_URL_DOMAIN: `http://192.168.1.243`, // TODO: can remove this later if wanted
+  // SOCKET_DEV_URL_DOMAIN: `http://192.168.1.243`, // At office IP. TODO: can remove this later if wanted
+  // SOCKET_DEV_URL_DOMAIN: `192.168.10.184`, // At home IP. TODO: can remove this later if wanted
 };
 
 export default socketConfig;
